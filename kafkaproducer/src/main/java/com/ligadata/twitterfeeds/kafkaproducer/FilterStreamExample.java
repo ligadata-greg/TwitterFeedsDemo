0f1de95dec0cbfad2a87f397d6dad648e5c828f8
Ahmed AlSarie <ahmedy@ligadata.com>
2015-04-28 18:30:21 +0300

    adding timestamp parameter to the new PMML

--- kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/FilterStreamExample.java
+++ kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/FilterStreamExample.java
@@ -13,7 +13,6 @@
 
 package com.ligadata.twitterfeeds.kafkaproducer;
 
-import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
@@ -21,54 +20,46 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.text.DateFormat;
 
 public class FilterStreamExample implements Runnable {
 
 	private static final String COMMA_DELIMITER = ",";
 	private String consumerSecret;
-	private String consumerKey; 
-	private String token; 
-	private String secret; 
-	private String hashtag;
+	private String consumerKey;
+	private String token;
+	private String secret;
+	private String csvHashtags;
 	
+
 	public FilterStreamExample(String consumerKey, String consumerSecret,
-			String token, String secret, String hashtag)
-	{
-		this.consumerSecret=consumerSecret;
-		this.consumerKey=consumerKey;
-		this.token=token;
-		this.secret=secret;
-		this.hashtag=hashtag;
+			String token, String secret, String csvHashtags) {
+		this.consumerSecret = consumerSecret;
+		this.consumerKey = consumerKey;
+		this.token = token;
+		this.secret = secret;
+		this.csvHashtags = csvHashtags;
 	}
-	
-	public void run(){
+
+	public void run() {
 		final BlockingQueue<String> queue = new LinkedBlockingQueue<String>(
 				10000);
 		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
-		SimpleDateFormat dateFormat = new SimpleDateFormat(
-				"yyyy-MM-dd HH-mm-ss");
 		Date date = new Date();
-
+		String DateToStr;
 		// add some track terms
-		endpoint.trackTerms(Lists.newArrayList("twitterapi", hashtag));
+		// endpoint.trackTerms(Lists.newArrayList("twitterapi", hashtag));
+		List<String> list = Arrays.asList(csvHashtags.split(","));
+		System.out.println("hashtags size: " + list.size());
+		System.out.println("hashtags list: " + list);
+		endpoint.trackTerms(list);
 
 		Authentication auth = new OAuth1(consumerKey, consumerSecret, token,
 				secret);
@@ -84,58 +75,66 @@ public class FilterStreamExample implements Runnable {
 
 		final KafkaProducer producer = new KafkaProducer();
 
-			@SuppressWarnings("static-access")
-				com.ligadata.twitterfeeds.zookeeperclient.Client c = new com.ligadata.twitterfeeds.zookeeperclient.Client();
-				try {
-					
-					if(!c.hashTagExists(hashtag)){
-						c.createHashTag(hashtag, "true");
-					}else{
-						c.setHashTag(hashtag, "true");
-					}
-					
-					while (c.getHashTag(hashtag).equals("true")) {
-						JSONObject json;
-					    String msg = null;
-						msg = queue.take();
+		// @SuppressWarnings("static-access")
+		// com.ligadata.twitterfeeds.zookeeperclient.Client c = new
+		// com.ligadata.twitterfeeds.zookeeperclient.Client();
+		try {
 
-						if (msg == null) {
-							System.out.println("Did not receive a message in 5 seconds");
+			// if(!c.hashTagExists(hashtag)){
+			// c.createHashTag(hashtag, "true");
+			// }else{
+			// c.setHashTag(hashtag, "true");
+			// }
+			//
+			// while (c.getHashTag(hashtag).equals("true")) {
+			while (true) {
+				JSONObject json;
+				String msg = null;
+				msg = queue.take();
+
+				if (msg == null) {
+					System.out
+							.println("Did not receive a message in 5 seconds");
+				} else {
+					StringBuffer str = new StringBuffer();
+					try {
+
+						json = new JSONObject(msg);
+						str.append("System.twittermsg");
+						// str.append("System.twittermsg");
+						str.append(COMMA_DELIMITER);
+						if (json.has("id")) {
+							str.append(json.get("id"));
 						} else {
-							StringBuffer str = new StringBuffer();
-							try {
-
-								json = new JSONObject(msg);
-								str.append("System.PlusStringStringTestMsg");
-								str.append(COMMA_DELIMITER);
-								if (json.has("id")) {
-									str.append(json.get("id"));
-								} else {
-									str.append("0");
-								}
-								str.append(COMMA_DELIMITER);
-								if (json.has("text")) {
-									str.append(json.get("text").toString().replace(",", " "));
-								} else {
-									str.append("empty");
-								}
-
-								producer.send(str.toString());
-
-							} catch (JSONException e) {
-								e.printStackTrace();
-								continue;
-							}
-
-							System.out.println(str.toString());
+							str.append("0");
 						}
+						str.append(COMMA_DELIMITER);
+						if (json.has("text")) {
+							str.append(json.get("text").toString()
+									.replace(",", " "));
+						} else {
+							str.append("empty");
+						}
+						DateToStr = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(date);
+						str.append(COMMA_DELIMITER);
+						str.append(DateToStr);
 
+						 producer.send(str.toString());
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+						continue;
 					}
-					c.close();
-					client.stop();
-					producer.close();
-				} catch (Exception e) {
-					e.printStackTrace();
+
+					System.out.println(str.toString());
 				}
+
+			}
+			// c.close();
+			// client.stop();
+			// producer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/Main.java
+++ kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/Main.java
@@ -1,24 +1,26 @@
 package com.ligadata.twitterfeeds.kafkaproducer;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-//		FilterStreamExample f = new FilterStreamExample(
-//				"JET4XHa2TpAdcpSKY1r6gn47Y",
-//				"u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
-//				"334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
-//				"cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB", "#jordan");
-		
-		new SampleStreamExample().run("JET4XHa2TpAdcpSKY1r6gn47Y",
-				"u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
-				"334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
-				"cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB"); 
-//		Thread t = new Thread(f); 
-//		t.start();
-//		
+		if (args.length > 0) {
+
+			FilterStreamExample f = new FilterStreamExample(
+					"JET4XHa2TpAdcpSKY1r6gn47Y",
+					"u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
+					"334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
+					"cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB", args[0]);
+
+			Thread t = new Thread(f);
+			t.start();
+		} else {
+			throw new IllegalArgumentException();
+		}
+		// new SampleStreamExample().run("JET4XHa2TpAdcpSKY1r6gn47Y",
+		// "u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
+		// "334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
+		// "cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB");
+		//
 	}
 
 }
