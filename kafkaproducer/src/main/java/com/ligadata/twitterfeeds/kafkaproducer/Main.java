b4a37bcb007f2a17f6f550bafbc8c7f2c1b1bcf6
ligadata-saleh <saleh@ligadata.com>
2015-05-10 13:28:52 +0300

    read from file

--- kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/Main.java
+++ kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/Main.java
@@ -1,18 +1,26 @@
 package com.ligadata.twitterfeeds.kafkaproducer;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length > 0) {
+			List<String> list = Arrays.asList(args[0].split(","));
 
-			FilterStreamExample f = new FilterStreamExample(
-					"JET4XHa2TpAdcpSKY1r6gn47Y",
-					"u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
-					"334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
-					"cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB",args[0]);// "apple"
-
-			Thread t = new Thread(f);
-			t.start();
+			for (String filepath : list) {
+				ReadFromFile f = new ReadFromFile(filepath);
+				Thread t = new Thread(f);
+				t.start();
+			}
+			
+			// FilterStreamExample f = new FilterStreamExample(
+			// "JET4XHa2TpAdcpSKY1r6gn47Y",
+			// "u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
+			// "334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
+			// "cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB",args[0]);//
+			// "apple"
 		} else {
 			throw new IllegalArgumentException();
 		}
@@ -20,7 +28,7 @@ public class Main {
 		// "u0EDXjhzUeRECuV2PiPFgJgEpdZUUD79eGGcXEHmXdiHG5sYr7",
 		// "334088612-wYvtA22ZnLkhNgsXheM8FJMU8Zu3guuraiEgQbiX",
 		// "cM4lMELZxP0xpX2SY7tNDD7JMwkEdwc4e5ELg3Tx5HiiB");
-		//
+
 	}
 
 }
---
+++ kafkaproducer/src/main/java/com/ligadata/twitterfeeds/kafkaproducer/ReadFromFile.java
@@ -0,0 +1,46 @@
+package com.ligadata.twitterfeeds.kafkaproducer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFromFile implements Runnable {
+	private BufferedReader br;
+	private StringBuilder sb;
+	private String line;
+	private KafkaProducer producer;
+
+	public ReadFromFile(String filePath) {
+		try {
+			br = new BufferedReader(new FileReader(filePath));
+			line = br.readLine();
+			producer = new KafkaProducer();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (true) {
+			if (line != null) {
+				try {
+					line = br.readLine();
+					
+					if(line.startsWith("System.twittermsg")){
+						sb = new StringBuilder();
+						sb.append(line);
+					}else{
+						sb.append(line);
+					}
+					producer.send(sb.toString());
+					System.out.println(sb.toString());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
