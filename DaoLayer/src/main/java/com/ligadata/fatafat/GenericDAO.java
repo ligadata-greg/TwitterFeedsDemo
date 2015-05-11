21f3e0a361694b6a0d71da98325df50fc9414c34
Ahmed AlSarie <ahmedy@ligadata.com>
2015-05-11 11:09:02 +0300

    Modifying connection string

--- DaoLayer/src/main/java/com/ligadata/fatafat/GenericDAO.java
+++ DaoLayer/src/main/java/com/ligadata/fatafat/GenericDAO.java
@@ -4,17 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class GenericDAO {
-	public final String URL = "jdbc:mysql://localhost:3306/";
-//	private final static String URL = "jdbc:mysql://fatafat.ligadata.com:3306/";
+//	public final String URL = "jdbc:mysql://localhost:3306/";
+	private final static String URL = "jdbc:mysql://fatafat.ligadata.com:3306/";
 
-//	private final static String SCHEMA_NAME = "demo";
-	private final static String SCHEMA_NAME = "myschema";
+	private final static String SCHEMA_NAME = "demo";
+//	private final static String SCHEMA_NAME = "myschema";
 
-//	public final String USERNAME = "demo";
-	private final static String USERNAME = "rootConn";
+	public final String USERNAME = "demo";
+//	private final static String USERNAME = "rootConn";
 	
-//	public final String PASSWORD = "demo123";
-	private final static String PASSWORD = "root";
+	public final String PASSWORD = "demo123";
+//	private final static String PASSWORD = "root";
 
 	public static String getSCHEMA_NAME() {
 		return SCHEMA_NAME;
